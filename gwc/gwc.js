c84d170f65c86d12389573cb251d684ca57d3c2c
zhangjunming2010 <18696785349@163.com>
2016-10-27 17:07:44 +0800

    完善购物车 增加左滑显示删除按钮

--- gwc/gwc.js
+++ gwc/gwc.js
@@ -30,6 +30,8 @@ function AutoResizeImage(objImg) {
 define(function(require) {
 	var $ = require("jquery");
 	var justep = require("$UI/system/lib/justep");
+	var touch = require("./touch");
+	
 	var Model = function() {
 		this.callParent();
 	};
@@ -84,6 +86,53 @@ define(function(require) {
 		var cartData = this.comp("cartData");
 		cartData.clear();
 		cartData.loadData(tmp);
+	};
+	
+	Model.prototype.swipeDelete = function(){
+		var newsContentID = this.getIDByXID('content2');
+		var swipeAreaSize = 100;
+		var dx;
+		touch.on('#' + newsContentID, 'touchstart', function(ev){
+			//ev.preventDefault();
+			var $swipNode = $('#' + newsContentID + " li").has(ev.target);
+			dx = parseInt($swipNode.attr('swipeX') || "0");
+		});
+		
+		touch.on('#'+ newsContentID, 'drag', function(ev){
+			var $swipNode = $('#' + newsContentID + " li").has(ev.target);
+			if(ev.direction === "left" || ev.direction === "right"){
+				dx = dx || 0;
+				var offx = dx + ev.x;
+				if(offx < swipeAreaSize*-1){
+					offx = swipeAreaSize*-1;
+				}
+				if(offx > 0){
+					offx = 0;
+				}
+				if(ev.direction === "right" && $swipNode.hasClass('x-swipe-out')){
+					$swipNode.css({'transform':'translate3d('+offx+'px,0,0)'});
+				}else if(ev.direction === "left" && (!$swipNode.hasClass('x-swipe-out'))){
+					$swipNode.css({'transform':'translate3d('+offx+'px,0,0)'});
+				}
+			}
+		});
+		touch.on('#'+ newsContentID, 'dragend', function(ev){
+			var $swipNode = $('#' + newsContentID + " li").has(ev.target);
+			if(ev.direction === "left"){
+				$swipNode.css({'transform':'translate3d(-'+swipeAreaSize+'px,0,0)'});
+				$swipNode.addClass('x-swipe-out');
+				dx = swipeAreaSize*-1;
+			}else if(ev.direction === "right"){
+				$swipNode.removeClass('x-swipe-out');
+				$swipNode.css({'transform':'translate3d(0,0,0)'});
+				dx = 0;
+			}
+			$swipNode.attr('swipeX',dx);
+		});
+	};
+
+	Model.prototype.modelLoad = function(event){
+		this.swipeDelete();
 	};
 
 	return Model;
